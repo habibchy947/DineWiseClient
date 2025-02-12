e17d1dab159a276209ff2e9b8a9531f3e7bccf85
Jihanchy <habibullahalquaderi2005@gmail.com>
2025-02-12 16:19:29 +0600

    customize navbar

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -7,7 +7,7 @@ const MainLayout = () => {
     return (
         <div className='font-raleWay max-w-screen-2xl mx-auto bg-slate-50 dark:bg-gray-900'>
             {/* navbar */}
-                <div className='dark:bg-gray-950 sticky top-0 z-20 backdrop:blur-xl bg-transparent bg-opacity-95 bg-white'>
+                <div className='dark:bg-gray-900 dark:drop-shadow-2xl sticky top-0 z-20 backdrop:blur-xl bg-transparent bg-opacity-95 bg-white'>
                 <Navbar></Navbar>
                 </div>
             <div className='min-h-[calc(100vh-306px)]'>
