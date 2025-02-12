a267986adc4c51bf0a02324a3cfe3dddf9265b55
Jihanchy <habibullahalquaderi2005@gmail.com>
2025-02-12 16:50:25 +0600

    customize loader

--- src/Shared/Loading.jsx
+++ src/Shared/Loading.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { RotatingLines } from 'react-loader-spinner';
+import { TbFidgetSpinner } from "react-icons/tb";
+import { ImSpinner2 } from "react-icons/im";
 const Loading = () => {
     return (
         <div className='flex justify-center items-center min-h-[200px]'>
-            <RotatingLines
+            {/* <RotatingLines
                 visible={true}
                 height="96"
                 width="96"
@@ -13,7 +15,9 @@ const Loading = () => {
                 ariaLabel="rotating-lines-loading"
                 wrapperStyle={{}}
                 wrapperClass=""
-            />
+            /> */}
+            {/* <TbFidgetSpinner className='animate-spin text-4xl text-orange-500' /> */}
+            <ImSpinner2 className='animate-spin text-4xl text-orange-500' />
         </div>
     );
 };
